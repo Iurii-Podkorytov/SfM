55105149e254f34fe259d0be20ba8af45f9416de
Iurii-Podkorytov <i.podkorytov@innopolis.university>
2024-11-25 12:16:56 +0300

    fixed nonlinear triangulation

--- structure_from_motion.py
+++ structure_from_motion.py
@@ -16,113 +16,100 @@ class SfM:
         self.camera_translations = [np.zeros((3, 1))]
         self.points_3d = []
 
-
     def _get_features(self, im1, im2):
         kp1, des1 = self.detector.detectAndCompute(im1, None)
         kp2, des2 = self.detector.detectAndCompute(im2, None)
         return kp1, des1, kp2, des2
     
-    # def _get_matched_points(self, kp1, kp2, des1, des2):
-    #     matches = self.matcher.match(des1, des2)
-
-    #     pts1 = np.float32([kp1[m.queryIdx].pt for m in matches])
-    #     pts2 = np.float32([kp2[m.trainIdx].pt for m in matches])
-
-    #     return pts1, pts2
-
-    
-    def _get_matched_points(self, kp1, kp2, des1, des2):
+    def _get_matched_points(self, kp1, kp2, des1, des2, img1=None, img2=None):
         matches = self.matcher.knnMatch(des1, des2, k=2)  # k=2 for ratio test
 
         good_matches = []
+        mask = [[0, 0] for _ in range(len(matches))] 
         pts1 = []
         pts2 = []
 
-        for m, n in matches:
-            if m.distance < 0.75 * n.distance:  # Ratio test (adjust 0.75 as needed)
+        for i, (m, n) in enumerate(matches):
+            if m.distance < 0.65 * n.distance:
                 good_matches.append(m)
                 pts1.append(kp1[m.queryIdx].pt)
                 pts2.append(kp2[m.trainIdx].pt)
+                mask[i] = [1, 0]
+
+        plot_matches(img1, img2, kp1, kp2, matches, mask)
         
         return np.float32(pts1), np.float32(pts2)
 
     def _project_points(self, X, R, t):
         projected_points = self.K @ (R @ X.T + t)
-        projected_points = projected_points[:2,:] / projected_points[2,:]
-        return projected_points.T
+        projected_points = cv2.convertPointsFromHomogeneous(projected_points.T)[:,0,:]
+        return projected_points
+    
     def _reprojection_error(self, X, R1, t1, R2, t2, pts1, pts2):
         X = X.reshape(-1, 3)
         projected_pts1 = self._project_points(X, R1, t1)
         projected_pts2 = self._project_points(X, R2, t2)
-        error = np.concatenate((projected_pts1 - pts1.reshape(-1,2), projected_pts2 - pts2.reshape(-1,2)), axis=1).flatten()
+        error = np.sum((projected_pts1 - pts1)**2 + (projected_pts2 - pts2)**2)
         return error
-    def _triangulation(self, kp1, kp2, T_1, T_2):
-        kp1_3D = np.ones((3, kp1.shape[0]))
-        kp2_3D = np.ones((3, kp2.shape[0]))
-        kp1_3D[0], kp1_3D[1] = kp1[:, 0].copy(), kp1[:, 1].copy()
-        kp2_3D[0], kp2_3D[1] = kp2[:, 0].copy(), kp2[:, 1].copy()
-        X = cv2.triangulatePoints(T_1[:3], T_2[:3], kp1_3D[:2], kp2_3D[:2])
-        X /= X[3]
-        return X[:3]
     
-    def _nonlinear_triangulation(self, R1, t1, R2, t2, pts1, pts2):
-        T1 = self._to_homogeneous_matrix(R1, t1)
-        T2 = self._to_homogeneous_matrix(R2, t2)
-        points3D_initial = self._triangulation(pts1, pts2, T1, T2)
-        
-        initial_guess = points3D_initial.flatten()
-        result = least_squares(self._reprojection_error, initial_guess,
-                            args=(R1, t1, R2, t2, pts1.reshape(-1,2), pts2.reshape(-1,2)),
-                            method='lm', xtol=1e-7)
+    def _triangulate(self, pts1, pts2, R1, t1, R2, t2):
+        img1ptsHom = cv2.convertPointsToHomogeneous(pts1)[:,0,:]
+        img2ptsHom = cv2.convertPointsToHomogeneous(pts2)[:,0,:]
 
-        optimized_points = result.x.reshape(-1, 3)
-        return optimized_points
+        img1ptsNorm = (np.linalg.inv(self.K).dot(img1ptsHom.T)).T
+        img2ptsNorm = (np.linalg.inv(self.K).dot(img2ptsHom.T)).T
 
+        img1ptsNorm = cv2.convertPointsFromHomogeneous(img1ptsNorm)[:,0,:]
+        img2ptsNorm = cv2.convertPointsFromHomogeneous(img2ptsNorm)[:,0,:]
+
+        pts4d = cv2.triangulatePoints(np.hstack((R1,t1)),np.hstack((R2,t2)),
+                                        img1ptsNorm.T,img2ptsNorm.T)
+        pts3d = cv2.convertPointsFromHomogeneous(pts4d.T)[:,0,:]
+        return pts3d
+
+    def nonlinear_triangulation(self, points_3d, R1, t1, R2, t2, pts1, pts2):
+        X0 = points_3d.flatten()
+        optimized_params = least_squares(
+            self._reprojection_error, X0, args=(R1, t1, R2, t2, pts1, pts2),
+            max_nfev=50
+        )
+        optimized_points_3d = optimized_params.x.reshape(-1, 3)
+        return optimized_points_3d
+    
     def _load_images(self, folder_path):
-        image_paths = sorted([os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(".jpg")])
+        image_paths = sorted([os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(".jpg") or f.endswith(".png")] )
         self.images = [cv2.imread(p, cv2.IMREAD_GRAYSCALE) for p in image_paths]
-    
-    def _to_homogeneous_matrix(self, R, t):
-        T = np.eye(4)
-        T[:3, :3] = R
-        T[:3, 3] = t.flatten()
-        return T
 
     def reconstruct(self, images_folder):
         self._load_images(images_folder)
         for i in range(len(self.images)-1):
+            # Detect fratures on two consecutive pictures and match them
             kp1, des1, kp2, des2 = self._get_features(self.images[i], self.images[i+1])
-            pts1, pts2 = self._get_matched_points(kp1, kp2, des1, des2)
+            pts1, pts2 = self._get_matched_points(kp1, kp2, des1, des2, self.images[i], self.images[i+1])
             
-            fundamental_matrix, mask = cv2.findFundamentalMat(pts1, pts2, cv2.RANSAC, 4, 0.99)
-            essential_matrix = self.K.T @ fundamental_matrix @ self.K
-            _, R, t, mask = cv2.recoverPose(essential_matrix, pts1, pts2)
-            # _, R, t, mask = cv2.recoverPose(essential_matrix, pts1, pts2, mask=mask)
-            # pts1 = pts1[mask.ravel() > 0]
-            # pts2 = pts2[mask.ravel() > 0]
-            if i == 0: self.points_2d.append(pts1)
+            # Initial triangulation
+            E, mask = cv2.findEssentialMat(pts1, pts2, self.K, method=cv2.RANSAC, prob=0.999, threshold=1.0)
+            pts1 = pts1[mask.ravel() == 1]
+            pts2 = pts2[mask.ravel() == 1]
+            _, R, t, _ = cv2.recoverPose(E, pts1, pts2, self.K)
+            points_3d = self._triangulate(pts1, pts2, self.camera_rotations[-1], self.camera_translations[-1], R, t)
             
-            T1 = np.eye(4)
-            T2 = self._to_homogeneous_matrix(R, t)
-            points_3d_init = self._triangulation(pts1, pts2, T1, T2)
-            
-            _, rvec, t, _ = cv2.solvePnPRansac(points_3d_init.T, pts2, self.K, self.dist)
-
-            rvec, t = cv2.solvePnPRefineLM(points_3d_init.T, pts2, self.K, self.dist, rvec, t)
+            # Use 3d points to refine pose estimation
+            _, rvec, t, _ = cv2.solvePnPRansac(points_3d, pts2, self.K, self.dist)
+            rvec, t = cv2.solvePnPRefineLM(points_3d, pts2, self.K, self.dist, rvec, t)
             R, _ = cv2.Rodrigues(rvec)
+            
+            # Use initial guess for nonlinear triangulation
+            points_3d = self.nonlinear_triangulation(points_3d, self.camera_rotations[-1], self.camera_translations[-1], R, t, pts1, pts2)
 
+            if i == 0: self.points_2d.append(pts1)
             self.points_2d.append(pts2)
             self.camera_rotations.append(R @ self.camera_rotations[-1])
             self.camera_translations.append(R @ self.camera_translations[-1] + t)
-
-            self.points_3d.append(self._nonlinear_triangulation(
-                self.camera_rotations[0], self.camera_translations[0], 
-                # self.camera_rotations[-2], self.camera_translations[-2], 
-                self.camera_rotations[-1], self.camera_translations[-1],
-                pts1, pts2
-            ))
-            
+            self.points_3d.append(points_3d)
+                    
         return np.vstack(self.points_3d)
+
 
 def plot_3d_points(points3D):
     fig = plt.figure()
@@ -133,6 +120,21 @@ def plot_3d_points(points3D):
     ax.set_zlabel('Z')
     plt.title('Triangulated 3D Points')
     plt.show(block=True)
+
+def plot_matches(img1, img2, kp1, kp2, matches, mask):
+    matched = cv2.drawMatchesKnn(img1, 
+                                kp1, 
+                                img2, 
+                                kp2, 
+                                matches,
+                                outImg=None, 
+                                matchColor=(0, 155, 0), 
+                                singlePointColor=(0, 255, 255), 
+                                matchesMask=mask, 
+                                flags=0) 
+    plt.title("Matched Features")
+    plt.imshow(matched)
+    plt.show()
 
 def plot_3d_points_and_cameras(points3D, Rs, ts):  # Combined plotting function
     fig = plt.figure()
@@ -153,14 +155,15 @@ def plot_3d_points_and_cameras(points3D, Rs, ts):  # Combined plotting function
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
     plt.title('Triangulated 3D Points and Camera Poses')
-    ax.legend() # Add a legend
     plt.show(block=True)
+
 
 K = np.array([[1.59877590e+03, 0, 5.07401915e+02], [0, 1.57986433e+03, 7.23899817e+02], [0, 0, 1]])
 dist =  np.array([1.01762294e-01, -1.85222000e+00, -1.95598585e-02, -2.43105406e-03, 4.57588149e+00])
 
 sfm = SfM(cv2.SIFT_create(), cv2.BFMatcher(normType=cv2.NORM_L2), K, dist)
-# sfm = SfM(cv2.SIFT_create(), cv2.BFMatcher(normType=cv2.NORM_L2, crossCheck=True), K, dist)
-points3d = sfm.reconstruct("images")
-# plot_3d_points(points3d)
-plot_3d_points_and_cameras(points3d, sfm.camera_rotations, sfm.camera_translations)
+
+result = sfm.reconstruct("benchy")
+
+# plot_3d_points_and_cameras(result, sfm.camera_rotations, sfm.camera_translations)
+plot_3d_points(result)
